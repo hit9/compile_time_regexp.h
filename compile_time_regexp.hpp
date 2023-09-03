26e63e8be5b7f9922e2bb5ec0886cd868c4a4871
hit9 <hit9@icloud.com>
2023-09-03 20:12:25 +0800

    Add a link onto the header comments

--- compile_time_regexp.hpp
+++ compile_time_regexp.hpp
@@ -1,5 +1,6 @@
 // Copyright (c) Chao Wang, hit9@icloud.com, 2023.
 // Compile time regular expression engine.
+// https://github.com/hit9/compile_time_regexp.hpp/tree/master
 // vim: foldmethod=marker foldenable
 
 #ifndef __COMPILETIME_REGEXP_H__
